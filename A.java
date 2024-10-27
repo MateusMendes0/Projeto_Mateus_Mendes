eed45fd1bc879530c531f303f4f6ab3334eb540f
Mateus Mendes <matreuusmendes@gmail.com>
2024-10-27 20:18:27 -0300

    Adicionando Classes A e B

---
+++ A.java
@@ -0,0 +1,23 @@
+public class A {
+
+    private int A1;
+    private float A2;
+
+
+    public int getA1(){
+        return A1;
+    }
+
+    public float getA2(){
+        return A2;
+    }
+
+    public void MA1(){
+        System.out.print(getA1());
+    }
+
+    public void MA2(){
+        System.out.print(getA2());
+    }    
+    
+}
---
+++ B.java
@@ -0,0 +1,22 @@
+public class B {
+    
+    int B1;
+    float B2;
+
+
+    public int getB1(){
+        return B1;
+    }
+
+    public float getB2(){
+        return B2;
+    }
+
+    public void MB1(){
+        System.out.print(getB1());;
+    }
+
+    public void MB2(){
+        System.out.print(getB2());;
+    }    
+}
