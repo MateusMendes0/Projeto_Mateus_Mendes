1d709bdb0b371f1896a59968d6ba69ba627d6bac
Mateus Mendes <matreuusmendes@gmail.com>
2024-10-27 21:40:04 -0300

    Criação da Classe D

---
+++ D.java
@@ -0,0 +1,18 @@
+public class D {
+    
+    String D1;
+    boolean D2;
+
+    public D(String D1, boolean D2){
+        this.D1 = D1;
+        this.D2 = D2;
+    }
+
+    public void MD1(){
+        System.out.printf("D1 : %s \nD2: %b", D1, D2);
+    }
+
+    public boolean MD2(){
+        return D2;
+    }
+}
