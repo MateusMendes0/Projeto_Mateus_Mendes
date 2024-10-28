b27ffeea7df97222384029f78e71a37d664a7283
Mateus Mendes <matreuusmendes@gmail.com>
2024-10-27 21:32:22 -0300

    Alteração das Classes B e C

--- A.java
+++ A.java
@@ -12,6 +12,14 @@ public class A {
         return A2;
     }
 
+    public void setA1(int A1){
+        this.A1 = A1;
+    }
+
+    public void setA2(float A2){
+        this.A2 = A2;
+    }
+    
     public void MA1(){
         System.out.print(getA1());
     }
--- B.java
+++ B.java
@@ -1,7 +1,7 @@
 public class B {
     
-    int B1;
-    float B2;
+    private int B1;
+    private float B2;
 
 
     public int getB1(){
@@ -12,11 +12,23 @@ public class B {
         return B2;
     }
 
+    public void setB1(int B1){
+        this.B1 = B1;
+    }
+
+    public void setB2(float B2){
+        this.B2 = B2;
+    }
+
     public void MB1(){
-        System.out.print(getB1());;
+        System.out.print(getB1());
     }
 
     public void MB2(){
-        System.out.print(getB2());;
-    }    
+        System.out.print(getB2());
+    }
+
+    public void MB3(){
+        System.out.println("MB3");
+    }
 }
